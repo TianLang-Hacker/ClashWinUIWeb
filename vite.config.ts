fde00fb4d41854fd1c395f5be1f0315530888445
TianLang Hacker <3071571983@qq.com>
2026-05-22 06:36:40 +0800

    set base URL

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/ClashWinUI/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
